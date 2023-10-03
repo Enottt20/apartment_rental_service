ecd5b950aa46dcf6f8e4eb657c06d3f970fe381d
Gigabooster <levcorp03@gmail.com>
2023-10-03 22:58:18 +0300

    POST fix

--- Services/Favorite service/app/database/models.py
+++ Services/Favorite service/app/database/models.py
@@ -2,15 +2,15 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, JSON
 from sqlalchemy.orm import relationship
 from .database import Base
 
-class Apartment(Base):
-    __tablename__ = 'apartments'
-
-    id = Column(Integer, primary_key=True, index=True)
-    address = Column(String, index=True)
-    rooms = Column(Integer)
-    area = Column(Integer)
-
-    favorite_item = relationship("FavoriteItem", back_populates="apartment")
+# class Apartment(Base):
+#     __tablename__ = 'apartments'
+#
+#     id = Column(Integer, primary_key=True, index=True)
+#     address = Column(String, index=True)
+#     rooms = Column(Integer)
+#     area = Column(Integer)
+#
+#     favorite_item = relationship("FavoriteItem", back_populates="apartment")
 
 
 class FavoriteItem(Base):
@@ -19,6 +19,6 @@ class FavoriteItem(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     description = Column(String)
-    apartment_id = Column(Integer, ForeignKey('apartments.id'))
+    #apartment_id = Column(Integer, ForeignKey('apartments.id'))
 
-    apartment = relationship("Apartment", back_populates="favorite_item")
+    #apartment = relationship("Apartment", back_populates="favorite_item")
--- Services/Favorite service/app/schemas.py
+++ Services/Favorite service/app/schemas.py
@@ -1,19 +1,19 @@
 from pydantic import BaseModel
 
-class Apartment(BaseModel):
-    id: int
-    address: str
-    rooms: int
-    area: int
-
-    class Config:
-        from_attributes = True
+# class Apartment(BaseModel):
+#     id: int
+#     address: str
+#     rooms: int
+#     area: int
+#
+#     class Config:
+#         from_attributes = True
 
 class FavoriteItem(BaseModel):
     id: int
     name: str
     description: str
-    apartment_id: int
+    #apartment_id: int
 
     class Config:
         from_attributes = True
