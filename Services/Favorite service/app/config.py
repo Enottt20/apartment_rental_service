9d79aedee0b8ff169a6b6d5b83c332e036d1fcb9
Gigabooster <levcorp03@gmail.com>
2023-10-08 21:27:32 +0300

    update config

--- Services/Favorite service/app/config.py
+++ Services/Favorite service/app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
-    POSTGRES_DSN: str
+    POSTGRES_DSN: str = "postgresql://postgres:1111@postgresql:5432/postgres"
 
     class Config:
         env_file = ".env"  # Указываем имя файла .env
