dd2df21f6a155b84ff016a4e54f0980d204b43dd
Enottt20 <levcorp03@gmail.com>
2023-11-10 11:54:28 +0300

    bug fix

--- Services/notification-service/app/m.py
+++ Services/notification-service/app/m.py
@@ -2,8 +2,11 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from logging import getLogger
+import config
+
 
 logger = getLogger("notification-service")
+cfg: config.Config = config.load_config()
 
 class EmailSender():
     def __init__(self, smtp_username, smtp_password, smtp_server="smtp.gmail.com", smtp_port=587):
@@ -16,9 +19,11 @@ class EmailSender():
             self.server = smtplib.SMTP(smtp_server, smtp_port)
             self.server.starttls()
             self.server.login(smtp_username, smtp_password)
+            print('соединение с сервером установленно')
             logger.info("соединение с сервером установленно")
 
         except:
+            print('соединение с сервером не установленно')
             logger.error("соединение с сервером не установленно")
 
 
@@ -33,6 +38,14 @@ class EmailSender():
 
             self.server.sendmail(self.smtp_username, recipient_email, msg.as_string())
 
+            self.server.quit()
+
             logger.info("Сообщение отправлено успешно.")
+            print('Сообщение отправлено успешно.')
         except:
             logger.error("Сообщение не было отправленно")
+            print('Сообщение не было отправленно')
+
+
+m = EmailSender('gigabooster@yandex.ru', 'gjfddhdgre22', 'smtp.yandex.ru', 587)
+m.send_message('t', 't', 'levkin03@mail.ru')
